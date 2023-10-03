9ae679c4115ca8d81e670d4cdcf9cb77bcc36942
ankul <awasthi.ankul121@gmail.com>
2023-10-03 16:40:11 +0530

    Reverse Number

--- 1.py
+++ 1.py
@@ -1,10 +1,11 @@
 #write a program reverse of a number 
 def reverse_num(num):
-    rev=0
-    while num!=0:
-        ld=num%10
-        rev=rev*10+ld
-        num=num//10 
+    rev=0   
+    while num!=0:   
+        ld = num%10   
+        rev = rev*10+ld    
+        num = num//10 
     return rev 
 print(reverse_num(199))    
+
     
